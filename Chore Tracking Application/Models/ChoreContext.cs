0144809bbecbe78ed6c77edb8bb307922291f681
Candace Williams <candace.williams94@hotmail.com>
2021-10-27 18:31:28 -0400

    update chore context and add EntityFrameworkCore.

--- Chore Tracking Application/Models/ChoreContext.cs
+++ Chore Tracking Application/Models/ChoreContext.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Chore_Tracking_Application.Models
 {
-    public class ChoreContext
+    public class ChoreContext : DbContext
     {
+        public ChoreContext(DbContextOptions<ChoreContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Chore> Chores { get; set; }
     }
 }
